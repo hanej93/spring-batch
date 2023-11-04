861472730ec5f263fcfd748d00c02be7c542bd92
EJ <hanej93@naver.com>
2023-11-04 13:29:36 +0900

    DB 스키마 생성 및 이해 (1)

--- src/main/java/io/springbatch/springbatchlecture/SpringBatchApplication.java
+++ src/main/java/io/springbatch/springbatchlecture/SpringBatchApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableBatchProcessing
+// Spring Batch 5에서는 기본 설정이 백오프이고, DefaultBatchConfiguration로 백오프를 설정해줘야 한다.
+// @EnableBatchProcessing
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
