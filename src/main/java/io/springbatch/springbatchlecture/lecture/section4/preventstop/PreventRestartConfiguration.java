638094dd97502b053c1a3e5c8bcf12b942a7c611
EJ <hanej93@naver.com>
2023-11-12 22:00:23 +0900

    SimpleJob - incrementer()

--- src/main/java/io/springbatch/springbatchlecture/lecture/section4/preventstop/PreventRestartConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/lecture/section4/preventstop/PreventRestartConfiguration.java
@@ -1,4 +1,4 @@
-package io.springbatch.springbatchlecture;
+package io.springbatch.springbatchlecture.lecture.section4.preventstop;
 
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
@@ -9,13 +9,12 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Configuration
+// @Configuration
 public class PreventRestartConfiguration {
 
 	private final JobRepository jobRepository;
@@ -56,8 +55,6 @@ public class PreventRestartConfiguration {
 	public Step step3() {
 		return new StepBuilder("step3", jobRepository)
 			.tasklet((contribution, chunkContext) -> {
-				chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
-				contribution.setExitStatus(ExitStatus.STOPPED);
 				System.out.println("step3 was executed");
 				return RepeatStatus.FINISHED;
 			}, transactionManager)
