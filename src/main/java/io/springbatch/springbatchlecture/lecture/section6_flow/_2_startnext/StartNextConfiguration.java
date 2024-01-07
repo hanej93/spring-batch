839c7b22239e6c746d1a56af6aa1b7b22b3ce8eb
EJ <hanej93@naver.com>
2024-01-08 00:53:11 +0900

    FlowJob - start() / next()

--- src/main/java/io/springbatch/springbatchlecture/lecture/section6_flow/_2_startnext/StartNextConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/lecture/section6_flow/_2_startnext/StartNextConfiguration.java
@@ -1,8 +1,10 @@
-package io.springbatch.springbatchlecture.lecture.section6_flow._1_flowjob;
+package io.springbatch.springbatchlecture.lecture.section6_flow._2_startnext;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -13,8 +15,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-// @Configuration
-public class FlowJobConfiguration {
+@Configuration
+public class StartNextConfiguration {
 
 	private final JobRepository jobRepository;
 	private final PlatformTransactionManager transactionManager;
@@ -22,12 +24,28 @@ public class FlowJobConfiguration {
 	@Bean
 	public Job batchJob() {
 		return new JobBuilder("batchJob", jobRepository)
-			.start(step1())
-			.on("COMPLETED").to(step3())
-			.from(step1())
-			.on("FAILED").to(step2())
+			.start(flowA())
+			.next(step3())
+			.next(flowB())
+			.next(step6())
 			.end()
 			.build();
+	}
+
+	@Bean
+	public Flow flowA() {
+		return new FlowBuilder<Flow>("flowA")
+			.start(step1())
+			.next(step2())
+			.end();
+	}
+
+	@Bean
+	public Flow flowB() {
+		return new FlowBuilder<Flow>("flowB")
+			.start(step4())
+			.next(step5())
+			.end();
 	}
 
 	@Bean
@@ -35,7 +53,6 @@ public class FlowJobConfiguration {
 		return new StepBuilder("step1", jobRepository)
 			.tasklet((contribution, chunkContext) -> {
 				System.out.println("step1 has executed");
-				// throw new RuntimeException("step1 was failed");
 				return RepeatStatus.FINISHED;
 			}, transactionManager)
 			.build();
@@ -52,10 +69,41 @@ public class FlowJobConfiguration {
 	}
 
 	@Bean
-	private Step step3() {
+	public Step step3() {
 		return new StepBuilder("step3", jobRepository)
 			.tasklet((contribution, chunkContext) -> {
 				System.out.println("step3 has executed");
+				return RepeatStatus.FINISHED;
+			}, transactionManager)
+			.build();
+	}
+
+	@Bean
+	public Step step4() {
+		return new StepBuilder("step4", jobRepository)
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step4 has executed");
+				throw new RuntimeException("step4 was failed");
+				// return RepeatStatus.FINISHED;
+			}, transactionManager)
+			.build();
+	}
+
+	@Bean
+	public Step step5() {
+		return new StepBuilder("step5", jobRepository)
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step5 has executed");
+				return RepeatStatus.FINISHED;
+			}, transactionManager)
+			.build();
+	}
+
+	@Bean
+	public Step step6() {
+		return new StepBuilder("step6", jobRepository)
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step6 has executed");
 				return RepeatStatus.FINISHED;
 			}, transactionManager)
 			.build();
