35d7680118682a4fdcff9541a316813b921359c0
EJ <hanej93@naver.com>
2024-01-08 01:50:42 +0900

    Transition - on() / to() / stop(), fail(), end(), stopAndRestart()

--- src/main/java/io/springbatch/springbatchlecture/lecture/section6_flow/_4_transition/TransitionConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/lecture/section6_flow/_4_transition/TransitionConfiguration.java
@@ -1,11 +1,9 @@
-package io.springbatch.springbatchlecture.lecture.section6_flow._3_batchstatusexitstatus;
+package io.springbatch.springbatchlecture.lecture.section6_flow._4_transition;
 
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -16,25 +14,27 @@ import org.springframework.transaction.PlatformTransactionManager;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-// @Configuration
-public class BatchStatusExitStatusConfiguration {
+@Configuration
+public class TransitionConfiguration {
 
 	private final JobRepository jobRepository;
 	private final PlatformTransactionManager transactionManager;
-
-	// @Bean
-	// public Job batchJob() {
-	// 	return new JobBuilder("batchJob", jobRepository)
-	// 		.start(step1())
-	// 		.next(step2())
-	// 		.build();
-	// }
 
 	@Bean
 	public Job batchJob() {
 		return new JobBuilder("batchJob", jobRepository)
 			.start(step1())
-			.on("FAILED").to(step2())
+				.on("FAILED")
+				.to(step2())
+				.on("FAILED")
+				.stop()
+			.from(step1())
+				.on("*")
+				.to(step3())
+				.next(step4())
+			.from(step2())
+				.on("*")
+				.to(step5())
 			.end()
 			.build();
 	}
@@ -55,6 +55,37 @@ public class BatchStatusExitStatusConfiguration {
 		return new StepBuilder("step2", jobRepository)
 			.tasklet((contribution, chunkContext) -> {
 				System.out.println("step2 has executed");
+				contribution.setExitStatus(ExitStatus.FAILED);
+				return RepeatStatus.FINISHED;
+			}, transactionManager)
+			.build();
+	}
+
+	@Bean
+	public Step step3() {
+		return new StepBuilder("step3", jobRepository)
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step3 has executed");
+				return RepeatStatus.FINISHED;
+			}, transactionManager)
+			.build();
+	}
+
+	@Bean
+	public Step step4() {
+		return new StepBuilder("step4", jobRepository)
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step4 has executed");
+				return RepeatStatus.FINISHED;
+			}, transactionManager)
+			.build();
+	}
+
+	@Bean
+	public Step step5() {
+		return new StepBuilder("step5", jobRepository)
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step5 has executed");
 				return RepeatStatus.FINISHED;
 			}, transactionManager)
 			.build();
