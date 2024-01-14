e5cf701993e32f9ced27e45ab9d9f4b8b0660255
EJ <hanej93@naver.com>
2024-01-14 09:42:49 +0900

    SimpleFlow 예제

--- src/main/java/io/springbatch/springbatchlecture/lecture/section6_flow/_9_simpleflowexam/SimpleFlowConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/lecture/section6_flow/_9_simpleflowexam/SimpleFlowConfiguration.java
@@ -1,4 +1,4 @@
-package io.springbatch.springbatchlecture.lecture.section6_flow._8_simpleflow;
+package io.springbatch.springbatchlecture.lecture.section6_flow._9_simpleflowexam;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -15,7 +15,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-// @Configuration
+@Configuration
 public class SimpleFlowConfiguration {
 
 	private final JobRepository jobRepository;
@@ -24,17 +24,38 @@ public class SimpleFlowConfiguration {
 	@Bean
 	public Job batchJob() {
 		return new JobBuilder("batchJob", jobRepository)
-			.start(flow())
-			.next(step3())
+			.start(flow1())
+				.on("COMPLETED")
+				.to(flow2())
+			.from(flow1())
+				.on("FAILED")
+				.to(flow3())
 			.end()
 			.build();
 	}
 
 	@Bean
-	Flow flow() {
-		return new FlowBuilder<Flow>("flow")
+	Flow flow1() {
+		return new FlowBuilder<Flow>("flow1")
 			.start(step1())
 			.next(step2())
+			.end();
+	}
+
+	@Bean
+	Flow flow2() {
+		return new FlowBuilder<Flow>("flow2")
+			.start(flow3())
+			.next(step5())
+			.next(step6())
+			.end();
+	}
+
+	@Bean
+	Flow flow3() {
+		return new FlowBuilder<Flow>("flow3")
+			.start(step3())
+			.next(step4())
 			.end();
 	}
 
@@ -53,7 +74,8 @@ public class SimpleFlowConfiguration {
 		return new StepBuilder("step2", jobRepository)
 			.tasklet((contribution, chunkContext) -> {
 				System.out.println("step2 has executed");
-				return RepeatStatus.FINISHED;
+				throw new RuntimeException("step2 was failed");
+				// return RepeatStatus.FINISHED;
 			}, transactionManager)
 			.build();
 	}
@@ -63,6 +85,36 @@ public class SimpleFlowConfiguration {
 		return new StepBuilder("step3", jobRepository)
 			.tasklet((contribution, chunkContext) -> {
 				System.out.println("step3 has executed");
+				return RepeatStatus.FINISHED;
+			}, transactionManager)
+			.build();
+	}
+
+	@Bean
+	public Step step4() {
+		return new StepBuilder("step4", jobRepository)
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step4 has executed");
+				return RepeatStatus.FINISHED;
+			}, transactionManager)
+			.build();
+	}
+
+	@Bean
+	public Step step5() {
+		return new StepBuilder("step5", jobRepository)
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step5 has executed");
+				return RepeatStatus.FINISHED;
+			}, transactionManager)
+			.build();
+	}
+
+	@Bean
+	public Step step6() {
+		return new StepBuilder("step6", jobRepository)
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step6 has executed");
 				return RepeatStatus.FINISHED;
 			}, transactionManager)
 			.build();
