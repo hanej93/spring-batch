d12d200800fd02ce0d4dfbc71f660b53f897aa8a
EJ <hanej93@naver.com>
2024-02-03 22:36:56 +0900

    DB - JdbcCursorItemReader

---
+++ src/main/java/io/springbatch/springbatchlecture/lecture/section8_itemreader/_7_jdbccursoritemreader/Customer.java
@@ -0,0 +1,12 @@
+package io.springbatch.springbatchlecture.lecture.section8_itemreader._7_jdbccursoritemreader;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String birthdate;
+}
--- src/main/java/io/springbatch/springbatchlecture/lecture/section8_itemreader/_7_jdbccursoritemreader/JdbcCursorConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/lecture/section8_itemreader/_7_jdbccursoritemreader/JdbcCursorConfiguration.java
@@ -1,4 +1,6 @@
-package io.springbatch.springbatchlecture.lecture.section8_itemreader._6_jsonitemreader;
+package io.springbatch.springbatchlecture.lecture.section8_itemreader._7_jdbccursoritemreader;
+
+import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -7,22 +9,23 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.json.JacksonJsonObjectReader;
-import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-// @Configuration
-public class JsonConfiguration {
+@Configuration
+public class JdbcCursorConfiguration {
+
+	public static final int CHUNK_SIZE = 10;
 
 	private final JobRepository jobRepository;
 	private final PlatformTransactionManager transactionManager;
+	private final DataSource dataSource;
 
 	@Bean
 	public Job batchJob() {
@@ -35,7 +38,7 @@ public class JsonConfiguration {
 	@Bean
 	public Step step1() {
 		return new StepBuilder("step1", jobRepository)
-			.<Customer, Customer>chunk(5, transactionManager)
+			.<Customer, Customer>chunk(CHUNK_SIZE, transactionManager)
 			.reader(customerItemReader())
 			.writer(customerItemWriter())
 			.build();
@@ -43,10 +46,13 @@ public class JsonConfiguration {
 
 	@Bean
 	public ItemReader<? extends Customer> customerItemReader() {
-		return new JsonItemReaderBuilder<Customer>()
-			.name("jsonReader")
-			.resource(new ClassPathResource("customer.json"))
-			.jsonObjectReader(new JacksonJsonObjectReader<>(Customer.class))
+		return new JdbcCursorItemReaderBuilder<Customer>()
+			.name("jdbcCursorItemReader")
+			.fetchSize(CHUNK_SIZE)
+			.sql("select id, firstName, lastName, birthdate from customer where firstName like ? order by lastName, firstName")
+			.beanRowMapper(Customer.class)
+			.queryArguments("A%")
+			.dataSource(dataSource)
 			.build();
 	}
 
