bb323c66c38ebb1537c7a0bc3ad39c249f9db88b
EJ <hanej93@naver.com>
2024-02-25 19:25:00 +0900

    어플리케이션 예제 (7)

--- src/main/java/io/springbatch/springbatchlecture/lecture/section15_batch_core/batch/chunk/writer/ApiItemWriter1.java
+++ src/main/java/io/springbatch/springbatchlecture/lecture/section15_batch_core/batch/chunk/writer/ApiItemWriter1.java
@@ -3,13 +3,16 @@ package io.springbatch.springbatchlecture.lecture.section15_batch_core.batch.chu
 import java.util.List;
 
 import org.springframework.batch.item.Chunk;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.core.io.FileSystemResource;
 
 import io.springbatch.springbatchlecture.lecture.section15_batch_core.batch.domain.ApiRequestVO;
 import io.springbatch.springbatchlecture.lecture.section15_batch_core.batch.domain.ApiResponseVO;
 import io.springbatch.springbatchlecture.lecture.section15_batch_core.service.AbstractApiService;
 
-public class ApiItemWriter1 implements ItemWriter<ApiRequestVO> {
+public class ApiItemWriter1 extends FlatFileItemWriter<ApiRequestVO> {
 
 	private final AbstractApiService apiService;
 
@@ -22,5 +25,12 @@ public class ApiItemWriter1 implements ItemWriter<ApiRequestVO> {
 		List<? extends ApiRequestVO> items = chunk.getItems();
 		ApiResponseVO responseVO = apiService.service(items);
 		System.out.println("responseVO = " + responseVO);
+		items.forEach(item -> item.setApiResponseVO(responseVO));
+
+		super.setResource(new FileSystemResource("C:\\Users\\hanej\\Desktop\\Study\\정수원\\spring-batch\\springbatch\\src\\main\\resources\\product1.txt"));
+		super.open(new ExecutionContext());
+		super.setLineAggregator(new DelimitedLineAggregator<>());
+		super.setAppendAllowed(true);
+		super.write(chunk);
 	}
 }
--- src/main/java/io/springbatch/springbatchlecture/lecture/section15_batch_core/scheduler/FileSchJob.java
+++ src/main/java/io/springbatch/springbatchlecture/lecture/section15_batch_core/scheduler/FileSchJob.java
@@ -1,12 +1,16 @@
 package io.springbatch.springbatchlecture.lecture.section15_batch_core.scheduler;
 
 import java.util.Date;
+import java.util.List;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
@@ -20,6 +24,7 @@ public class FileSchJob extends QuartzJobBean {
 
 	private final Job fileJob;
 	private final JobLauncher jobLauncher;
+	private final JobExplorer jobExplorer;
 
 	@SneakyThrows
 	@Override
@@ -30,6 +35,21 @@ public class FileSchJob extends QuartzJobBean {
 			.addLong("id", new Date().getTime())
 			.addString("requestDate", requestDate)
 			.toJobParameters();
+
+		int jobInstanceCount = (int) jobExplorer.getJobInstanceCount(fileJob.getName());
+		List<JobInstance> jobInstances = jobExplorer.getJobInstances(fileJob.getName(), 0, jobInstanceCount);
+
+		if (!jobInstances.isEmpty()) {
+			for (JobInstance jobInstance : jobInstances) {
+				List<JobExecution> jobExecutions = jobExplorer.getJobExecutions(jobInstance);
+				boolean hasRequestDateInJobExecution = jobExecutions.stream()
+					.anyMatch(
+						jobExecution -> requestDate.equals(jobExecution.getJobParameters().getString("requestDate")));
+				if (hasRequestDateInJobExecution) {
+					throw new JobExecutionException(requestDate + " already exists");
+				}
+			}
+		}
 
 		jobLauncher.run(fileJob, jobParameters);
 	}
